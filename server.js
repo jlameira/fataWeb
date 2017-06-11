d1b0491cf55a594cf84c4c957a6e23459a879f0c
jlameira <jlameira@gmail.com>
2017-06-11 20:55:26 -0300

    agora vai

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 })
