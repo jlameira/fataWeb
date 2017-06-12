b88fdc1476e7550b008d6c1bd7db2665599ff4c7
jlameira <jlameira@gmail.com>
2017-06-11 23:37:56 -0300

    dashborda2

---
+++ src/dashboard2/dashboard2.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import axios from 'axios'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
+
+const BASE_URL = 'https://fatauni.herokuapp.com/fata/v1/public'
+
+export default class Dashboard2 extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentWillMount() {
+    axios.get(`${BASE_URL}/company/findall`)
+      .then(resp => {
+        console.log(resp.data);
+        this.setState(resp.data[0])
+      })
+      .catch(e => {
+        debugger
+        console.log(e);
+      });
+  }
+
+  render() {
+    debugger
+    const { _00 } = this.state
+    return (
+      <div>
+        <ContentHeader title='Dashboard' small='Versão 1.0' />
+        <Content>
+          <Row>
+            <ValueBox cols='12 4' color='green' icon='bus' value={`${_00}`} text='20 vagas' />
+            <ValueBox cols='12 4' color='red' icon='bus' value='Van Barreiro' text='10 vagas' />
+            <ValueBox cols='12 4' color='blue' icon='bus' value='Van Gameleira' text='15 vagas' />
+          </Row>
+        </Content>
+      </div>
+    )
+  }
+
+}
+
+
--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
-import Dashboard from '../dashboard/dashboard'
+import Dashboard2 from '../dashboard2/dashboard2'
 import BillinCycle from '../billingCycle/billingCycle'
 import Company from '../company/company'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
+        <Route path='/' component={Dashboard2} />
         <Route path='/billingCycle' component={BillinCycle} />
         <Route path='/company' component={Company} />
         <Redirect from='*' to='/' />
