39459037bbc724e9cd35d90dac6744059622da6c
jlameira <jlameira@gmail.com>
2017-06-13 22:41:06 -0300

    company crud

--- src/company/company.jsx
+++ src/company/company.jsx
@@ -1,19 +1,48 @@
 import React, { Component } from 'react'
 
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 
+import Tabs from '../common/tab/tabs'
+import TabsContent from '../common/tab/tabsContent'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
+import { selectTab } from '../common/tab/tabActions'
+
 class Company extends Component {
+
+    componentWillMount() {
+        this.props.selectTab('tabList')
+
+    }
     render() {
         return (
             <div>
                 <ContentHeader title='Cadastro de Empresa' small='Cadastro' />
                 <Content>
-                    Cadastro de Empresa
-            </Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Altera' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Deletar</h1></TabContent>
+                        </TabsContent>
+                    </Tabs>
+
+                </Content>
             </div>
         )
     }
 }
-
-export default Company
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
+export default connect(null, mapDispatchToProps)(Company)
